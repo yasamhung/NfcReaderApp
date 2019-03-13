ec54bf0e9e454e1613fb8074d5b3ff11a4206c45
h.yasam <muratayasam@gmail.com>
2019-03-13 16:53:18 +0800

    add // https://mvnrepository.com/artifact/com.google.guava/guava     compile group: 'com.google.guava', name: 'guava', version: 'r05'

--- app/src/main/java/com/example/nfcreaderapp/record/UriRecord.java
+++ app/src/main/java/com/example/nfcreaderapp/record/UriRecord.java
@@ -1,6 +1,14 @@
 package com.example.nfcreaderapp.record;
 
+import com.google.common.collect.BiMap;
+
 public class UriRecord implements ParsedNdefRecord {
+
+    private static final String TAG = "UriRecord";
+    private static final String RECORD_TYPE = "UriRecord";
+
+    private static final BiMap <Byte, String> URI_PREFIX_MAP = null;
+
     @Override
     public String str() {
         return null;
